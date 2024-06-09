6706f956a2d515566c9b5f05a01a86b03532b059
ezequiel flores <eze53flores@gmail.com>
2024-06-09 11:29:36 +0200

    ejercicio de callbak

--- C8 Objects + Callbacks/callbacks/ejercicios/06.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/06.js
@@ -3,6 +3,13 @@ function map(array, cb) {
   // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
   // Retorna el nuevo arreglo.
   // Tu código:
+
+var nuevo_array = [];
+
+for(var i = 0; i < array.length; i++){
+  nuevo_array.push(cb(array[i]));
+}return nuevo_array;
+
 }
 
 module.exports = map;
--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,16 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+   var nu_array = [];
+
+   for(var i = 0; i < arrayOfStrings.length; i++){
+      if(arrayOfStrings[i][0]===`a` || arrayOfStrings[i][0] === `A`){
+         nu_array.push(arrayOfStrings[i]);
+      }
+   }
+   return nu_array;
+
 }
 
 module.exports = filter;
